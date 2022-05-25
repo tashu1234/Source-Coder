c6ae397ba2c21d074b7fc871f0ecbe133120799b
tashu1234 <99098395+tashu1234@users.noreply.github.com>
2022-05-25 13:40:15 +0530

    Add files via upload

---
+++ Source coder/script.js
@@ -0,0 +1,38 @@
+const button = document.getElementById('button')
+const toasts = document.getElementById('toasts')
+
+const messages = [
+    'Park-Jimin',
+    'Kim-Seok-Jin',
+    'Kim-Namjoon',
+    'Jeon-Jungkook',
+    'Min-Yoongi',
+    'Kim-Taehyung',
+    'Jung-Hosoek',
+]
+
+const types = ['info', 'success', 'error']
+
+button.addEventListener('click', () => createNotification())
+
+function createNotification(message = null, type = null) {
+    const notif = document.createElement('div')
+    notif.classList.add('toast')
+    notif.classList.add(type ? type : getRandomType())
+
+    notif.innerText = message ? message : getRandomMessage()
+
+    toasts.appendChild(notif)
+
+    setTimeout(() => {
+        notif.remove()
+    }, 3000)
+}
+
+function getRandomMessage() {
+    return messages[Math.floor(Math.random() * messages.length)]
+}
+
+function getRandomType() {
+    return types[Math.floor(Math.random() * types.length)]
+}
